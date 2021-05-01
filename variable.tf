a2722f303666438130419a1b82d971a5e901c11c
sample-qut <83010355+sample-qut@users.noreply.github.com>
2021-05-01 16:33:02 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,3 +2,8 @@ variable "terraform_service_account" {
   description = "Service account email of the account to impersonate to run Terraform."
   type        = string
 }
+
+variable "project_id" {
+  description = "Service account email of the account to impersonate to run Terraform."
+  default       = "cft-seed-50a5"
+}
