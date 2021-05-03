d83baa05397c4529182dc7700f1542fee3da1f36
sample-qut <83010355+sample-qut@users.noreply.github.com>
2021-05-03 13:36:25 +0530

    Update mani.tf

--- mani.tf
+++ mani.tf
@@ -1,47 +1,44 @@
-resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-  project      = var.project_id
- 
-  tags = ["foo", "bar"]
- 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
+resource "google_storage_bucket" "es_id" {
+  name                        = var.name
+  project                     = var.project_id
+  location                    = var.location
+  storage_class               = var.storage_class
+  uniform_bucket_level_access = var.bucket_policy_only
+  force_destroy               = var.force_destroy
+
+  versioning {
+    enabled = var.versioning
+  }
+
+  dynamic "retention_policy" {
+    for_each = var.retention_policy == null ? [] : [var.retention_policy]
+    content {
+      is_locked        = var.retention_policy.is_locked
+      retention_period = var.retention_policy.retention_period
     }
   }
- 
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
- 
-  network_interface {
-    network = "default"
- 
-    access_config {
-      // Ephemeral IP
+
+  dynamic "encryption" {
+    for_each = var.encryption == null ? [] : [var.encryption]
+    content {
+      default_kms_key_name = var.encryption.default_kms_key_name
     }
   }
- 
-  metadata = {
-    foo = "bar"
-  }
- 
-}
- 
-resource "google_storage_bucket" "auto-expire" {
-  name          = "auto-expiring-bucket"
-  location      = "US"
-  force_destroy = true
-  project = var.project_id
-  lifecycle_rule {
-    condition {
-      age = 3
-    }
-    action {
-      type = "Delete"
+
+  dynamic "lifecycle_rule" {
+    for_each = var.lifecycle_rules
+    content {
+      action {
+        type          = lifecycle_rule.value.action.type
+        storage_class = lookup(lifecycle_rule.value.action, "storage_class", null)
+      }
+      condition {
+        age                   = lookup(lifecycle_rule.value.condition, "age", null)
+        created_before        = lookup(lifecycle_rule.value.condition, "created_before", null)
+        with_state            = lookup(lifecycle_rule.value.condition, "with_state", lookup(lifecycle_rule.value.condition, "is_live", false) ? "LIVE" : null)
+        matches_storage_class = lookup(lifecycle_rule.value.condition, "matches_storage_class", null)
+        num_newer_versions    = lookup(lifecycle_rule.value.condition, "num_newer_versions", null)
+      }
     }
   }
 }
